586e7571065bc0da7c7de583591da6ca2831ec07
sidharth nayyar <sidnayyar1@gmail.com>
2020-05-20 10:04:12 -0400

    given id to all the UI layout essentials and working on mainActivity

--- app/src/main/java/com/example/gameone/MainActivity.java
+++ app/src/main/java/com/example/gameone/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.gameone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    public EditText editextemail,edittextpassword;
+    Button login;
+    TextView tvsignin;
+    FirebaseAuth mfirebaseauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+mfirebaseauth = FirebaseAuth.getInstance();
+editextemail = findViewById(R.id.edittextemail);
+edittextpassword = findViewById(R.id.edittextpassword);
+tvsignin = findViewById(R.id.tvsignin);
+login = findViewById(R.id.btnlogin);
 
 
     }
