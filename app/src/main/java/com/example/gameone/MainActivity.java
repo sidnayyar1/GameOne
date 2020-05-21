f9df758b031e8c88edc5305e63d46bead04db909
sidharth nayyar <sidnayyar1@gmail.com>
2020-05-21 02:41:38 -0400

    now working on login button.
    https://www.youtube.com/watch?v=V0ZrnL-i77Q
    link of the video for better understanding

--- app/src/main/java/com/example/gameone/MainActivity.java
+++ app/src/main/java/com/example/gameone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gameone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,7 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,7 +37,29 @@ login.setOnClickListener(new View.OnClickListener() {
     public void onClick(View v) {
         String Email = editextemail.getText().toString();
         String password = edittextpassword.getText().toString();
-        
+    if (Email.isEmpty()){
+        editextemail.setError("please enter the email");
+        editextemail.requestFocus();
+
+        } else if (password.isEmpty()){
+        edittextpassword.setError("please enter password");
+        edittextpassword.requestFocus();
+
+        }else if(Email.isEmpty() & password.isEmpty()){
+        Toast.makeText(MainActivity.this,"fields are empty",Toast.LENGTH_LONG);
+    }
+    else if(!Email.isEmpty() && password.isEmpty()){
+        mfirebaseauth.createUserWithEmailAndPassword(Email,password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()){
+                    Toast.makeText(MainActivity.this,"login unsuccesful.please try again later",Toast.LENGTH_LONG);
+                }else {
+
+                }
+            }
+        });
+    }
     }
 });
 
